48d0ba1ac11a515cb80a4f1a1b8e3ddee66ceb28
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-15 09:38:42 -0300

    Update programa04.py

--- LTP1-2020-2/Pratica04/programa04.py
+++ LTP1-2020-2/Pratica04/programa04.py
@@ -1,9 +1,19 @@
 #pede ao usuario para digitar um valor para idade
+
 idade = int(input('informe sua idade:'))
 
+
+
 #pergunta, no codigo, se a idade é maior ou igual a 18
+
 if idade >= 18:
-  #este codigo pertence ao bloco do if
-print('Voce possui 18 anos ou mais')
-print('Agora!Voce pode ver todas as paginas de jogos da Stream')
+
+#este codigo pertence ao bloco do if
+ print('Voce possui 18 anos ou mais')
+
+ print('Agora!Voce pode ver todas as paginas de jogos da Stream')
+
+else: 
+ print('volte quando for mais velho')
+
 print ('Final do programa')
