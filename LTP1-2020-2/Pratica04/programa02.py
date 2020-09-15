01777093cc39628ac50ccf11b578eaf32944d0ee
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-15 08:55:27 -0300

    Create programa02.py

---
+++ LTP1-2020-2/Pratica04/programa02.py
@@ -0,0 +1,12 @@
+#Operadores de divisão (/) o resto da divisão (%)
+#divisao = 15 / 2
+#resto = 15 / 2
+
+#print (' Divisao:', divisao)
+#print ('Resto:', resto)
+
+#Ler um numero para ver se ele é par ou impar
+numero = int(input('informe um numero:'))
+#Calcula o resto da divisao do numero por 2
+resto = numero % 2
+
