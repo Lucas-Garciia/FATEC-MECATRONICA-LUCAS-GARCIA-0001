597c8e8e6324cb3358e2935d63b64c90e07116ae
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-15 08:39:40 -0300

    Create programa01.py

---
+++ LTP1-2020-2/Pratica04/programa01.py
@@ -0,0 +1,10 @@
+senha_secreta = '123456'
+
+#pede para o usuario digitar uma senha 
+senha = input ('Informe a senha: ')
+
+#verifica se a senha do usuario esta compile
+if senha == senha_secreta:
+  print('Bem vindo Hackerman')
+else:
+  print('acesso negado!')
