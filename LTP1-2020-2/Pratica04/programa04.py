3d0ca994e3b3261bc2cf4870299744b41fe1be7b
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-15 09:19:49 -0300

    Update and rename programa05.py to programa04.py

---
+++ LTP1-2020-2/Pratica04/programa04.py
@@ -0,0 +1,9 @@
+#pede ao usuario para digitar um valor para idade
+idade = int(input('informe sua idade:'))
+
+#pergunta, no codigo, se a idade é maior ou igual a 18
+if idade >= 18:
+  #este codigo pertence ao bloco do if
+print('Voce possui 18 anos ou mais')
+print('Agora!Voce pode ver todas as paginas de jogos da Stream')
+print ('Final do programa')
--- LTP1-2020-2/Pratica04/programa05.py
+++
@@ -1,5 +0,0 @@
-#pede ao usuario para digitar um valor para idade
-idade = int(input('informe sua idade:'))
-
-#pergunta, no codigo, se a idade é maior ou igual a 18
-if idade >= 18
