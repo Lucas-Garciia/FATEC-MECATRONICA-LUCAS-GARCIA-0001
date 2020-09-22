c8d3e9559ad07565c5671cfd1d0cf0a183d61281
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-22 09:21:34 -0300

    Create Calculadora.py

---
+++ LTP1-2020-2/pratica05/Calculadora.py
@@ -0,0 +1,23 @@
+#CALCULADORA
+operação = input('informe a operacao desejada')
+
+if operacao == '+':
+  valor01 = float(input('valor 1:'))
+  valor02 = float(input('valor 2:'))
+  resultado = valor01 + valor02
+  print(resultado)
+elif operacao == '-':
+  valor01 = float(input('valor 1:'))
+  valor02 = float(input('valor 2:'))
+  resultado = valor01 - valor02
+  print(resultado)
+  elif operacao == '*':
+  valor01 = float(input('valor 1:'))
+  valor02 = float(input('valor 2:'))
+  resultado = valor01 * valor02
+  print(resultado)
+  elif operacao == '/':
+  valor01 = float(input('valor 1:'))
+  valor02 = float(input('valor 2:'))
+  resultado = valor01 / valor02
+  print(resultado)
