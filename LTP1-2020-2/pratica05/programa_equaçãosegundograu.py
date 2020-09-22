94d7faad48fbdcc2ae8a509abe604569b753c999
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-22 09:10:12 -0300

    Create programa_equaçãosegundograu.py

---
+++ LTP1-2020-2/pratica05/programa_equaçãosegundograu.py
@@ -0,0 +1,10 @@
+#EQUAÇAO DE SEGUNDO GRAU
+A = int(input('informe o valor de A'))
+B = int(input('informe o valor de B')) 
+C = int(input('informe o valor de C')) 
+
+Delta = (B*B)-4*A*C
+if Delta < 0:
+  print ('valor de Delta menor que Zero, nao possui raizes reais')
+elif Delta > 0:
+ 
