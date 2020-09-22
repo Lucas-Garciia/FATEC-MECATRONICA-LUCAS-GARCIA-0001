e3f35ccec849bb2adbb96199818241b588a672e9
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-22 09:45:35 -0300

    Update Calculadora.py

--- LTP1-2020-2/pratica05/Calculadora.py
+++ LTP1-2020-2/pratica05/Calculadora.py
@@ -1,5 +1,5 @@
 #CALCULADORA
-operação = input('informe a operacao desejada')
+operacao = input('informe a operacao desejada')
 
 if operacao == '+':
   valor01 = float(input('valor 1:'))
@@ -11,13 +11,18 @@ elif operacao == '-':
   valor02 = float(input('valor 2:'))
   resultado = valor01 - valor02
   print(resultado)
-  elif operacao == '*':
+elif operacao == '*':
   valor01 = float(input('valor 1:'))
   valor02 = float(input('valor 2:'))
   resultado = valor01 * valor02
   print(resultado)
-  elif operacao == '/':
+elif operacao == '/':
   valor01 = float(input('valor 1:'))
   valor02 = float(input('valor 2:'))
-  resultado = valor01 / valor02
-  print(resultado)
+  if valor02 == 0:
+    print('Tentativa de divisão por Zero')
+  else:
+    resultado = valor01 / valor02
+    print (resultado)
+else:
+  print('operação invalida')
