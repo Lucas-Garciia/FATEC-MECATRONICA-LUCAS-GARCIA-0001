2db174dda02ee71ae63a5f2d30d40ae2ad89655f
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-09-22 09:54:40 -0300

    Update programa_equaçãosegundograu.py

--- LTP1-2020-2/pratica05/programa_equaçãosegundograu.py
+++ LTP1-2020-2/pratica05/programa_equaçãosegundograu.py
@@ -7,4 +7,9 @@ Delta = (B*B)-4*A*C
 if Delta < 0:
   print ('valor de Delta menor que Zero, nao possui raizes reais')
 elif Delta > 0:
- 
+ x1 = (-B + Delta**0.5)/(2*A)
+   x2 = (-B - Delta**0.5)/(2*A)
+   print('Raizes:', x1, x2 )
+ else:
+   x2 = (-B - Delta**0.5)/(2*A)
+   print ('Raiz', x2)
