02cac160d8500b4396719b4455cd7b1750e5be4f
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-11-24 10:59:55 -0300

    Create prog03.py

---
+++ LTP1-2020-2/pratica13/prog03.py
@@ -0,0 +1,9 @@
+no1 = float(input("informe um valor"))
+no2 = float(input("informe outro valor"))
+
+if no1 > no2 :
+  print("maior", no1)
+elif no1 < no2 :
+  print ("maior", no2)
+else:
+  print("são iguais")
