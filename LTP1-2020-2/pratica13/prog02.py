32a9e7ec5049779b9eae178dedbea8a7ceb6ba66
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-11-24 10:52:59 -0300

    Create prog02.py

---
+++ LTP1-2020-2/pratica13/prog02.py
@@ -0,0 +1,9 @@
+no1 = int(input("informe um valor"))
+no2 = int(input("informe outro valor"))
+
+print("soma", no1+no2)
+print("produto:", no1*no2)
+print("divisao:", no1/no2)
+print("divisao inteira:", no1//no2)
+print("resto da divisao:", no1%no2)
+print("potenciação:",no1**no2)
