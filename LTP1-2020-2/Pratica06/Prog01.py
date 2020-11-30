c4c3fca6ca34277cfb16adcb66fba7eaa08a9637
Lucas-Garciia <70581492+Lucas-Garciia@users.noreply.github.com>
2020-11-30 02:21:04 -0300

    Create Prog01.py

---
+++ LTP1-2020-2/Pratica06/Prog01.py
@@ -0,0 +1,8 @@
+print('Ola Mundo')
+
+#Isso é um comentário - não será executado
+#A função input() sempre retorna uma string/texto
+nome = input("Informe seu nome:")
+
+print("Ola", nome)
+© 2020 GitHub, Inc.
